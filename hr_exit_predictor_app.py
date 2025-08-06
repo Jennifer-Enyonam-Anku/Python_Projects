3660c853966d059d811c01777940ef80037e88bb
Anku Jennifer Enyonam <ankujenyonam5@gmail.com>
2025-08-06 23:19:08 +0000

    Update hr_exit_predictor_app.py

--- hr_exit_predictor_app.py
+++ hr_exit_predictor_app.py
@@ -23,7 +23,7 @@ st.markdown("""
             border-right: none;
         }
         html, body, [data-testid="stAppViewContainer"] > .main {
-            background-color: #16abc2 !important;
+            background-color: white !important;
             color: black !important;
         }
         .stSelectbox > div,
